426c540be33e9d4e5613fe65e2bd6deeee2dc38e
2kybe3 <kybe@kybe.xyz>
2026-05-13 15:27:35 +0200

    shift enter = meta_return

--- nixvim/plugins/orgmode.nix
+++ nixvim/plugins/orgmode.nix
@@ -4,9 +4,17 @@
     settings = {
       org_agenda_files = "~/orgfiles/**/*";
       org_default_notes_file = "~/orgfiles/refile.org";
-      mappings.org_return_uses_meta_return = true;
     };
     luaConfig.post = ''
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = 'org',
+        callback = function()
+          vim.keymap.set('i', '<S-CR>', '<cmd>lua require("orgmode").action("org_mappings.meta_return")<CR>', {
+            silent = true,
+            buffer = true,
+          })
+        end,
+      })
       vim.lsp.enable('org')
     '';
   };
