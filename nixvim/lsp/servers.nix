2bc5c19a1fcbf2887b4ded2ca87d1888d6b17506
2kybe3 <kybe@kybe.xyz>
2026-07-31 22:14:32 +0200

    pylsp -> basedpyright

--- nixvim/lsp/servers.nix
+++ nixvim/lsp/servers.nix
@@ -4,12 +4,12 @@
     servers = {
       # keep-sorted start block=yes
 
+      basedpyright.enable = true;
       bashls.enable = true;
       gopls.enable = true;
       jsonls.enable = true;
       just.enable = true;
       nil_ls.enable = true;
-      pylsp.enable = true;
       statix.enable = true;
       ts_ls.enable = true;
       yamlls.enable = true;
